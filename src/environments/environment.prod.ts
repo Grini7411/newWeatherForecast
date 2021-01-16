968477cfbcbbb063a87c79c8fb001a3a3175e173
Grini7411 <viperdavben@gmail.com>
2021-01-16 21:11:07 +0200

    add production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  api_key: 'Di8MDsAQ0cU6Ygb1yNgwNyvYN6jdOAvb',
+  google: {
+    googleKey: 'AIzaSyCfO37sg96QZdJQ4tfOTKdRGGKUI5HqbPM',
+    cx: 'ffa99d7347bf5feec',
+    c2coff: 1,
+    filter: 1,
+    imgColorType: 'color',
+  }
 };
